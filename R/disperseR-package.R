d349aae8d10d01e8b4b1e1f4998bfe40319a1600
Jonathan <zzards@proton.me>
2026-01-18 19:12:36 +0100

    Restore magrittr/maps, fix CI vignette handling
    - Restore magrittr to Imports with @importFrom %>%
    - Restore maps to Imports (required by ggplot2::map_data)
    - Remove unused ncdf4 from Imports
    - Add package documentation file with pipe import
    - Update CI to use --ignore-vignettes and build_args

---
+++ R/disperseR-package.R
@@ -0,0 +1,10 @@
+#' disperseR: Run HYSPLIT Many Times in Parallel
+#'
+#' @description
+#' disperseR runs HYSPLIT dispersion models and calculates the HyADS
+#' exposure metric, aggregating results to ZIP code level.
+#'
+#' @importFrom magrittr %>%
+#' @name disperseR-package
+#' @aliases disperseR
+"_PACKAGE"
